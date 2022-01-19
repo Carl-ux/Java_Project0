8ecd2fc3a16f480493c2d11fc75a0417d0ccb70a
Carl <02carl1126@gmail.com>
2022-01-19 17:11:07 +0800

    Default Changelist

---
+++ day01/src/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ @author  carl
+ @version v1.0
+ My First Java Project!!
+ */
+
+public class HelloWorld {
+    /**
+     文档注释
+     */
+    public static void main(String[] args){
+        System.out.println("helloworld!");
+    }
+}
+//API Application Programming Interface  应用程序编程接口
